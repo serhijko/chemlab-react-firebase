0b5197d4a908e794935983ff549a16e1a0eee824
serhijko <sp_by@tut.by>
2020-07-03 13:13:54 -0300

    Password Forget (fix code)

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
+import useFormInput from '../../handlers/useFormInput';
 
 const PasswordForgetPage = () => (
-  <div>
-    <h1>PasswordForget</h1>
-  </div>
+    <div>
+        <h1>PasswordForget</h1>
+        <PasswordForgetForm />
+    </div>
 );
 
 const INITIAL_STATE = {
@@ -54,59 +56,6 @@ const PasswordForgetFormBase = props => {
 const PasswordForgetLink = () => (
     <p>
         <Link to={ROUTES.PASSWORD_FORGET}>Forget Password?</Link>
-    </p>
-);
-
-const INITIAL_STATE = {
-    email: '',
-    error: null,
-};
-
-const PasswordForgetFormBase = props => {
-    const [email, setEmail] = useState(INITIAL_STATE.email);
-    const [error, setError] = useState(INITIAL_STATE.error);
-
-    const onSubmit = event => {
-        props.firebase
-            .doPasswordReset(email)
-            .then(() => {
-                setEmail(INITIAL_STATE.email);
-                setError(INITIAL_STATE.error);
-            })
-            .catch(error => {
-                setError(error);
-            });
-
-        event.preventDefault();
-    }
-
-    const onChange = event => {
-        setEmail(event.target.value);
-    };
-
-    const isInvalid = email === '';
-
-    return (
-        <form onSubmit={onSubmit}>
-            <input
-                name="email"
-                value={email}
-                onChange={onChange}
-                type="text"
-                placeholder="Email Address"
-            />
-            <button disabled={isInvalid} type="submit">
-                Reset My Password
-            </button>
-
-            {error && <p>{error.message}</p>}
-        </form>
-    );
-};
-
-const PasswordForgetLink = () => (
-    <p>
-        <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
     </p>
 );
 

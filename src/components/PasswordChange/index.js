d42e5d6cbd1e62bd73deca5fddc8fd0258947561
serhijko <sp_by@tut.by>
2020-07-03 08:01:25 +0300

    Password Change

--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const PasswordChange = () => (
-  <div>
-    <h1>PasswordChange</h1>
-  </div>
-);
+import { withFirebase } from '../Firebase';
+import useFormInput from '../../handlers/useFormInput';
 
-export default PasswordChange;
+const INITIAL_STATE = {
+    passwordOne: '',
+    passwordTwo: '',
+    error: null,
+}
+
+const PasswordChangeForm = props => {
+    const [passwordOne, setPasswordOne] = useState(INITIAL_STATE.passwordOne);
+    const [passwordTwo, setPasswordTwo] = useState(INITIAL_STATE.passwordTwo);
+    const [error, setError] = useState(INITIAL_STATE.error);
+
+    const onSubmit = event => {
+        props.firebase
+            .doPasswordUpdate(passwordOne)
+            .then(() => {
+                setPasswordOne(INITIAL_STATE.passwordOne);
+                setPasswordTwo(INITIAL_STATE.passwordTwo);
+                setError(INITIAL_STATE.error);
+            })
+            .catch(error => {
+                setError(error);
+            });
+
+        event.preventDefault();
+    };
+
+    const isInvalid =
+        passwordOne !== passwordTwo || passwordOne === '';
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                {...useFormInput(passwordOne, setPasswordOne)}
+                type="password"
+                placeholder="New Password"
+            />
+            <input
+                {...useFormInput(passwordTwo, setPasswordTwo)}
+                type="password"
+                placeholder="Confirm New Password"
+            />
+            <button disabled={isInvalid} type="submit">
+                Reset My Password
+            </button>
+
+            {error && <p>{error.message}</p>}
+        </form>
+    );
+}
+
+export default withFirebase(PasswordChangeForm);

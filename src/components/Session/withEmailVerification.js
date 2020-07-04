eff8257b4467419e27c616f3920199703c300e68
serhijko <sp_by@tut.by>
2020-07-04 16:03:11 +0300

    Email Verification refactoring

--- src/components/Session/withEmailVerification.js
+++ src/components/Session/withEmailVerification.js
@@ -7,6 +7,36 @@ const needsEmailVerification = authUser =>
     authUser &&
     !authUser.emailVerified;
 
+const DefaultComponent = ({
+    buttonTitle = 'Send confirmation E-mail.',
+    children,
+    onClick,
+    disabled,
+}) => (
+    <div>
+        <p>{children}</p>
+        <button
+            type="button"
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {buttonTitle}
+        </button>
+    </div>
+);
+
+const MESSAGE_IF_EMAIL_IS_SENT = `
+    E-mail confirmation sent: Check your E-Mails (Spam
+    folder included) for a confirmation E-Mail.
+    Refresh this page once you confirmed your E-mail.
+`;
+
+const MESSAGE_IF_EMAIL_IS_NOT_SENT = `
+    Verify your E-mail: Check your E-Mails (Spam folder
+    included) for a confirmation E-Mail or send
+    another confirmation E-mail.
+`;
+
 const withEmailVerification = Component => {
     const WithEmailVerification = props => {
         const [isSent, setIsSent] = useState(false);
@@ -19,32 +49,19 @@ const withEmailVerification = Component => {
         };
 
         return (needsEmailVerification(authUser) ? (
-                <div>
-                    {isSent ? (
-                        <p>
-                            E-mail confirmation sent: Check your E-Mails (Spam
-                            folder included) for a confirmation E-Mail.
-                            Refresh this page once you confirmed your E-mail.
-                        </p>
-                    ) : (
-                        <p>
-                            Verify your E-mail: Check your E-Mails (Spam folder
-                            included) for a confirmation E-Mail or send
-                            another confirmation E-mail.
-                        </p>
-                    )}
-
-                    <button
-                        type="button"
-                        onClick={onSendEmailVerification}
-                        disabled={isSent}
-                    >
-                        Send confirmation E-mail.
-                    </button>
-                </div>
-            ) : (
-                <Component {...props} />
-            ));
+            <DefaultComponent
+                onClick={onSendEmailVerification}
+                disabled={isSent}
+            >
+                {isSent ? (
+                    MESSAGE_IF_EMAIL_IS_SENT
+                ) : (
+                    MESSAGE_IF_EMAIL_IS_NOT_SENT
+                )}
+            </DefaultComponent>
+        ) : (
+            <Component {...props} />
+        ));
     };
 
     return withFirebase(WithEmailVerification);

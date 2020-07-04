dc0161a3f38f0d3e5dadcabfec353d8f23b77ba8
serhijko <sp_by@tut.by>
2020-07-04 08:29:44 +0300

    Session Persistence

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -5,14 +5,16 @@ import { withFirebase } from '../Firebase';
 
 const withAuthentication = Component => {
     const WithAuthentication = props => {
-        const [authUser, setAuthUser] = useState(null);
+        const [authUser, setAuthUser] = useState(JSON.parse(localStorage.getItem('authUser')));
 
         useEffect(() => {
             const listener = props.firebase.onAuthUserListener(
                 authUser => {
+                    localStorage.setItem('authUser', JSON.stringify(authUser));
                     setAuthUser(authUser);
                 },
                 () => {
+                    localStorage.removeItem('authUser');
                     setAuthUser(null);
                 },
             );
